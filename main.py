cfd62902aed8274e0ad5eb231050c770a1b4039d
F-sama <67035576+F-sama@users.noreply.github.com>
2022-10-22 12:46:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-data = {"love_days":{"value":get_count_love()},"loveAnniversary_left":{"value":get_loveday()},"engage_days":{"value":get_count_engage()},"engageAnniversary_left":{"value":get_loveday()},"birthday_left":{"value":get_engageday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"love_days":{"value":get_count_love()},"loveAnniversary_left":{"value":get_loveday()},"engage_days":{"value":get_count_engage()},"engageAnniversary_left":{"value":get_engageday()},"birthday_left":{"value":get_engageday()},"words":{"value":get_words(), "color":get_random_color()}}
 res_baby = wm.send_template(user_id_baby, template_id, data)
 res_fly = wm.send_template(user_id_fly, template_id, data)
 print(res_baby)
