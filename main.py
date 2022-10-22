4b4d858aa203ba883f4ea93b008d198f8b66ea76
F-sama <67035576+F-sama@users.noreply.github.com>
2022-10-22 12:35:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ engage_date = os.environ['ENGAGE_DATE']
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
-user_id = os.environ["USER_ID"]
+user_id_fly = os.environ["USER_ID_FLY"]
+user_id_baby = os.environ["USER_ID_BABY"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
@@ -62,5 +63,7 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 data = {"love_days":{"value":get_count_love()},"loveAnniversary_left":{"value":get_loveday()},"engage_days":{"value":get_count_engage()},"engageAnniversary_left":{"value":get_loveday()},"birthday_left":{"value":get_engageday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+res_baby = wm.send_template(user_id_baby, template_id, data)
+res_fly = wm.send_template(user_id_fly, template_id, data)
+print(res_baby)
+print(res_fly)
